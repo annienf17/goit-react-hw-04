54b09c501e671a6b16650bca54ecfb7845c44afb
annienf17 <a.ulanskaxxi@gmail.com>
2024-09-04 15:43:35 +0100

    loader css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,10 @@
 import { TailSpin } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 export default function Loader() {
   return (
-    <div className="loader">
-      <TailSpin height="50" width="50" color="grey" ariaLabel="loading" />
+    <div className={css.loader}>
+      <TailSpin height="100" width="100" color="#1198f8" ariaLabel="loading" />
     </div>
   );
 }
